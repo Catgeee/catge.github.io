7417112e70c6d9e255aa49b6ceea9a6983a88c40
Catgeee <osoho_31@yahoo.com>
2021-12-24 05:27:52 +0800

    Added new files

---
+++ lab/lab12/index.js
@@ -0,0 +1,15 @@
+document.getElementById("click").onclick = click;
+
+var heads = 0;
+var tails = 0;
+function click() {
+  x = Math.floor(Math.random() * 2) == 0;
+  if (x) {
+    flip("Heads");
+  } else {
+    flip("Tails");
+  }
+}
+function flip(coin) {
+  document.getElementById("result").innerHTML = coin;
+}
